051a8bbca28e4e14a8f5d35b32783a113b93e3c3
gerson <l19091321@zacatepec.tecnm.com.mx>
2025-04-20 00:43:55 -0600

    config: de swagger y conexion a la base

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: '',
+    database: 'bdd_cursos_intersemestrales',
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: false, // ⚠️ false si ya tienes las tablas en MySQL
+  }),],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Sistema de Cursos Intersemestrales')
+    .setDescription('API para gestión de periodos, usuarios, cursos, inscripciones, evaluaciones y certificados')
+    .setVersion('1.0')
+    .addTag('NestJS + MySQL')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
